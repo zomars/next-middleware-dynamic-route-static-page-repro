7fb3711800517367d32a87038305a5f2c3f57668
zomars <zomars@me.com>
2022-07-27 18:14:10 -0600

    Adds locales

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,33 @@ function defineNextConfig(config) {
 }
 
 export default defineNextConfig({
+  i18n: {
+    defaultLocale: "en",
+    locales: [
+      "en",
+      "fr",
+      "it",
+      "ru",
+      "es",
+      "de",
+      "pt",
+      "ro",
+      "nl",
+      "pt-BR",
+      "es-419",
+      "ko",
+      "ja",
+      "pl",
+      "ar",
+      "iw",
+      "zh-CN",
+      "zh-TW",
+      "cs",
+      "sr",
+      "sv",
+      "vi",
+    ],
+  },
   reactStrictMode: true,
   swcMinify: true,
 });
